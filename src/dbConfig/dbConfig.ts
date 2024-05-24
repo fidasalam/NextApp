3ece77289c838ccdfe96fe065e857953fd2574e0
fida salam <149894458+fidasalam@users.noreply.github.com>
2024-05-24 15:12:45 +0530

    Update dbConfig.ts

--- src/dbConfig/dbConfig.ts
+++ src/dbConfig/dbConfig.ts
@@ -4,10 +4,7 @@ export async function connect() {
     
     try{
 
-        await mongoose.connect(process.env.MONGO_URI!, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true // Add this line to avoid deprecation warning
-          });
+        await mongoose.connect(process.env.MONGO_URI!)
           console.log('Database Connection Established');
 
     }catch(error){
